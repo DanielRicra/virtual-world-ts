642760f26ceae73a071cfd553e431717a45fff98
DanielRicra <daniel.int@outlook.com>
2023-12-30 16:24:49 -0500

    Remove buttons, to add and remove

--- src/main.ts
+++ src/main.ts
@@ -3,20 +3,9 @@ import { Point } from "./primitives/point";
 import { Segment } from "./primitives/segment";
 import config from "./config/constants";
 import "./style.css";
+import { GraphEditor } from "./graph-editor";
 
 const canvas = document.querySelector("#myCanvas") as HTMLCanvasElement;
-
-const addPointBtn = document.querySelector("#addPoint") as HTMLButtonElement;
-const addSegmentBtn = document.querySelector(
-  "#addSegment"
-) as HTMLButtonElement;
-const removeSegmentBtn = document.querySelector(
-  "#removeSegment"
-) as HTMLButtonElement;
-const removePointBtn = document.querySelector(
-  "#removePoint"
-) as HTMLButtonElement;
-const removeAllBtn = document.querySelector("#removeAll") as HTMLButtonElement;
 
 canvas.width = config.WIDTH;
 canvas.height = config.HEIGHT;
@@ -38,59 +27,12 @@ const s3 = new Segment(p1, p4);
 const s4 = new Segment(p2, p3);
 
 const graph = new Graph([p1, p2, p3, p4], [s1, s2, s3, s4]);
-graph.draw(context);
+const graphEditor = new GraphEditor(canvas, graph);
 
-addPointBtn.addEventListener("click", () => addRandomPoint(context));
-addSegmentBtn.addEventListener("click", () => addRandomSegment(context));
-removeSegmentBtn.addEventListener("click", () => removeRandomSegment(context));
-removePointBtn.addEventListener("click", () => removeRandomPoint(context));
-removeAllBtn.addEventListener("click", () => removeAll(context));
+animate();
 
-function addRandomPoint(context: CanvasRenderingContext2D) {
-  const success = graph.tryAddPoint(
-    new Point(Math.random() * config.WIDTH, Math.random() * config.HEIGHT)
-  );
-  context.clearRect(0, 0, config.WIDTH, config.HEIGHT);
-  graph.draw(context);
-  console.log(success);
-}
-
-function addRandomSegment(context: CanvasRenderingContext2D) {
-  const indexOne = Math.floor(Math.random() * graph.points.length);
-  const indexTwo = Math.floor(Math.random() * graph.points.length);
-
-  const success = graph.tryAddSegment(
-    new Segment(graph.points[indexOne], graph.points[indexTwo])
-  );
-  context.clearRect(0, 0, config.WIDTH, config.HEIGHT);
-  graph.draw(context);
-  console.log(success);
-}
-
-function removeRandomSegment(context: CanvasRenderingContext2D) {
-  if (graph.segments.length === 0) {
-    console.log("No segments to remove");
-  }
-
-  const index = Math.floor(Math.random() * graph.segments.length);
-  graph.removeSegment(graph.segments[index]);
-  context.clearRect(0, 0, config.WIDTH, config.HEIGHT);
-  graph.draw(context);
-}
-
-function removeRandomPoint(context: CanvasRenderingContext2D) {
-  if (graph.points.length === 0) {
-    console.log("No points to remove");
-  }
-
-  const index = Math.floor(Math.random() * graph.points.length);
-  graph.removePoint(graph.points[index]);
-  context.clearRect(0, 0, config.WIDTH, config.HEIGHT);
-  graph.draw(context);
-}
-
-function removeAll(context: CanvasRenderingContext2D) {
-  graph.dispose();
-  context.clearRect(0, 0, config.WIDTH, config.HEIGHT);
-  graph.draw(context);
+function animate() {
+  context?.clearRect(0, 0, config.WIDTH, config.HEIGHT);
+  graphEditor.display();
+  requestAnimationFrame(animate);
 }
