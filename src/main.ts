29a6f45e8faa739db9592cab2aa5ea1524b89cc9
DanielRicra <daniel.int@outlook.com>
2023-12-29 20:59:53 -0500

    Add buttons to add point and segment

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,18 @@
 import { Graph } from "./math/graph";
 import { Point } from "./primitives/point";
 import { Segment } from "./primitives/segment";
+import config from "./config/constants";
 import "./style.css";
 
 const canvas = document.querySelector("#myCanvas") as HTMLCanvasElement;
 
-canvas.width = 600;
-canvas.height = 600;
+const addPointBtn = document.querySelector("#addPoint") as HTMLButtonElement;
+const addSegmentBtn = document.querySelector(
+  "#addSegment"
+) as HTMLButtonElement;
+
+canvas.width = config.WIDTH;
+canvas.height = config.HEIGHT;
 
 const context = canvas.getContext("2d");
 
@@ -26,3 +32,27 @@ const s4 = new Segment(p2, p3);
 
 const graph = new Graph([p1, p2, p3, p4], [s1, s2, s3, s4]);
 graph.draw(context);
+
+addPointBtn.addEventListener("click", () => addRandomPoint(context));
+addSegmentBtn.addEventListener("click", () => addRandomSegment(context));
+
+function addRandomPoint(context: CanvasRenderingContext2D) {
+  const success = graph.tryAddPoint(
+    new Point(Math.random() * config.WIDTH, Math.random() * config.HEIGHT)
+  );
+  context.clearRect(0, 0, config.WIDTH, config.HEIGHT);
+  graph.draw(context);
+  console.log(success);
+}
+
+function addRandomSegment(context: CanvasRenderingContext2D) {
+  const indexOne = Math.floor(Math.random() * graph.points.length);
+  const indexTwo = Math.floor(Math.random() * graph.points.length);
+
+  const success = graph.tryAddSegment(
+    new Segment(graph.points[indexOne], graph.points[indexTwo])
+  );
+  context.clearRect(0, 0, config.WIDTH, config.HEIGHT);
+  graph.draw(context);
+  console.log(success);
+}
