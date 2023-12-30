ac588c0330adb80a9a21ff6a4db581e2226f0086
DanielRicra <daniel.int@outlook.com>
2023-12-29 21:28:19 -0500

    Update point.ts
    Add method `equals` to Point class to verify if two point have the same x and y

--- src/primitives/point.ts
+++ src/primitives/point.ts
@@ -7,11 +7,19 @@ export class Point {
     this.y = y;
   }
 
-  draw(context: CanvasRenderingContext2D, size: number = 18, color: string = "black") {
-    const radius = size /2;
+  equals(point: Point): boolean {
+    return this.x === point.x && this.y === point.y;
+  }
+
+  draw(
+    context: CanvasRenderingContext2D,
+    size: number = 18,
+    color: string = "black"
+  ) {
+    const radius = size / 2;
     context.beginPath();
     context.fillStyle = color;
-    context.arc(this.x, this.y, radius, 0, Math.PI * 2)
-    context.fill()
+    context.arc(this.x, this.y, radius, 0, Math.PI * 2);
+    context.fill();
   }
 }
