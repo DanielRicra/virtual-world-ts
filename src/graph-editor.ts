beda3f63d1b10109dc8bbb7fbc4bf3b4b1532fff
DanielRicra <daniel.int@outlook.com>
2024-01-02 16:18:28 -0500

    Update graph-editor.ts
    - Add attributes to know selected, hovered point, or if dragging.
    
    - add mouse events, for r and l click to add or remove a point.
    
    - add mouseup, contextmen and mousemove listeners

--- src/graph-editor.ts
+++ src/graph-editor.ts
@@ -1,14 +1,26 @@
 import type { Graph } from "./math/graph";
+import type { Utils } from "./math/utils";
 import { Point } from "./primitives/point";
 
 export class GraphEditor {
   canvas: HTMLCanvasElement;
   graph: Graph;
   context: CanvasRenderingContext2D;
+  selectedPoint: Point | null;
+  hoveredPoint: Point | null;
+  isDragging: boolean;
 
-  constructor(canvas: HTMLCanvasElement, graph: Graph) {
+  constructor(
+    canvas: HTMLCanvasElement,
+    graph: Graph,
+    private readonly utils: Utils
+  ) {
     this.canvas = canvas;
     this.graph = graph;
+    this.selectedPoint = null;
+    this.hoveredPoint = null;
+    this.utils = utils;
+    this.isDragging = false;
 
     this.context = canvas.getContext("2d") as CanvasRenderingContext2D;
 
@@ -17,12 +29,63 @@ export class GraphEditor {
 
   private addEventListeners() {
     this.canvas.addEventListener("mousedown", (event) => {
-      const pointFromMouse = new Point(event.offsetX, event.offsetY);
-      this.graph.addPoint(pointFromMouse);
+      if (event.button === 2) {
+        if (this.hoveredPoint) {
+          this.removePoint(this.hoveredPoint);
+        }
+      }
+
+      if (event.button === 0) {
+        const pointFromMouse = new Point(event.offsetX, event.offsetY);
+
+        if (this.hoveredPoint) {
+          this.selectedPoint = this.hoveredPoint;
+          this.isDragging = true;
+          return;
+        }
+
+        this.graph.addPoint(pointFromMouse);
+        this.selectedPoint = pointFromMouse;
+        this.hoveredPoint = pointFromMouse;
+      }
     });
+
+    this.canvas.addEventListener("mousemove", (event) => {
+      const pointFromMouse = new Point(event.offsetX, event.offsetY);
+      this.hoveredPoint = this.utils.getNearestPoint(
+        pointFromMouse,
+        this.graph.points,
+        10
+      );
+      if (this.isDragging && this.selectedPoint) {
+        this.selectedPoint.x = pointFromMouse.x;
+        this.selectedPoint.y = pointFromMouse.y;
+      }
+    });
+
+    this.canvas.addEventListener("contextmenu", (event) =>
+      event.preventDefault()
+    );
+
+    this.canvas.addEventListener("mouseup", () => (this.isDragging = false));
+  }
+
+  private removePoint(point: Point) {
+    this.graph.removePoint(point);
+    this.hoveredPoint = null;
+    if (this.selectedPoint == point) {
+      this.selectedPoint = null;
+    }
   }
 
   display() {
     this.graph.draw(this.context);
+    if (this.hoveredPoint) {
+      this.hoveredPoint.draw(this.context, { fill: true });
+    }
+
+    if (this.selectedPoint) {
+      this.selectedPoint.draw(this.context, { outline: true });
+    }
   }
 }
