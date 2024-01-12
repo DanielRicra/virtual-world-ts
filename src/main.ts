a2e4be3b6ee6c84cd4ee770435fe24f681f97c9f
DanielRicra <daniel.int@outlook.com>
2024-01-12 16:44:58 -0500

    Update main.ts
    - Remove default points and segments.
    
    - Load points and segments from local storage.
    
    - Implement 'dispose' and 'save' functions for the buttons with same name.

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,4 @@
 import { Graph } from "./math/graph";
-import { Point } from "./primitives/point";
-import { Segment } from "./primitives/segment";
 import config from "./config/constants";
 import "./style.css";
 import { GraphEditor } from "./graph-editor";
@@ -8,6 +6,12 @@ import { Utils } from "./math/utils";
 import { Viewport } from "./viewport";
 
 const canvas = document.querySelector("#myCanvas") as HTMLCanvasElement;
+
+const deleteBtn = document.querySelector("#deleteBtn") as HTMLButtonElement;
+const saveBtn = document.querySelector("#saveBtn") as HTMLButtonElement;
+
+deleteBtn.addEventListener("click", dispose);
+saveBtn.addEventListener("click", save);
 
 canvas.width = config.WIDTH;
 canvas.height = config.HEIGHT;
@@ -18,18 +22,11 @@ if (context === null) {
   throw new Error("Oops! The context is null.");
 }
 
-const p1 = new Point(200, 200);
-const p2 = new Point(500, 200);
-const p3 = new Point(400, 400);
-const p4 = new Point(100, 300);
-
-const s1 = new Segment(p1, p2);
-const s2 = new Segment(p1, p3);
-const s3 = new Segment(p1, p4);
-const s4 = new Segment(p2, p3);
+const graphString = localStorage.getItem("graph");
+const graphInfo = graphString ? JSON.parse(graphString) : null;
 
 const utils = new Utils();
-const graph = new Graph([p1, p2, p3, p4], [s1, s2, s3, s4]);
+const graph = graphInfo ? Graph.load(graphInfo) : new Graph();
 const viewport = new Viewport(canvas, utils);
 const graphEditor = new GraphEditor(viewport, graph, utils);
 
@@ -40,4 +37,12 @@ function animate() {
 
   graphEditor.display();
   requestAnimationFrame(animate);
+}
+
+function dispose() {
+  graphEditor.dispose();
+}
+
+function save() {
+  localStorage.setItem("graph", JSON.stringify(graph));
 }
