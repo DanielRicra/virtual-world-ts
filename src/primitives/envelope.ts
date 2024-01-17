6edb95eee3472a6ac4b8550c00e29fc627d00ceb
DanielRicra <daniel.int@outlook.com>
2024-01-16 19:49:06 -0500

    Add Envelope class

---
+++ src/primitives/envelope.ts
@@ -0,0 +1,41 @@
+import { Polygon, Segment } from ".";
+import { Utils } from "../math/utils";
+
+export class Envelope {
+  polygon: Polygon;
+
+  constructor(
+    private skeleton: Segment,
+    width: number,
+    roundness: number = 1,
+    private readonly utils: Utils
+  ) {
+    this.polygon = this.generatePolygon(width, roundness);
+  }
+
+  private generatePolygon(width: number, roundness: number) {
+    const { p1, p2 } = this.skeleton;
+
+    const radius = width / 2;
+    const alpha = this.utils.angle(this.utils.subtract(p1, p2));
+    const alpha_cw = alpha + Math.PI / 2;
+    const alpha_ccw = alpha - Math.PI / 2;
+
+    const points = [];
+    const step = Math.PI / Math.max(1, roundness);
+    const epsilon = step / 2;
+    for (let i = alpha_ccw; i <= alpha_cw + epsilon; i += step) {
+      points.push(this.utils.translate(p1, i, radius));
+    }
+
+    for (let i = alpha_ccw; i <= alpha_cw + epsilon; i += step) {
+      points.push(this.utils.translate(p2, Math.PI + i, radius));
+    }
+
+    return new Polygon(points);
+  }
+
+  draw(context: CanvasRenderingContext2D) {
+    this.polygon.draw(context);
+  }
+}
