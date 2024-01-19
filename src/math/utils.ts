1d90304a50a9343d12b4df701d8ef3bcb922036a
DanielRicra <daniel.int@outlook.com>
2024-01-19 11:44:56 -0500

    Update utils.ts to add new methods

--- src/math/utils.ts
+++ src/math/utils.ts
@@ -5,6 +5,10 @@ export class Utils {
     return Math.hypot(p1.x - p2.x, p1.y - p2.y);
   }
 
+  average(p1: Point, p2: Point): Point {
+    return new Point((p1.x + p2.x) / 2, (p1.y + p2.y) / 2);
+  }
+
   getNearestPoint(
     location: Point,
     points: Point[],
@@ -45,5 +49,29 @@ export class Utils {
 
   angle(p: Point) {
     return Math.atan2(p.y, p.x);
+  }
+
+  getIntersection(A: Point, B: Point, C: Point, D: Point) {
+    const tTop = (D.x - C.x) * (A.y - C.y) - (D.y - C.y) * (A.x - C.x);
+    const uTop = (C.y - A.y) * (A.x - B.x) - (C.x - A.x) * (A.y - B.y);
+    const bottom = (D.y - C.y) * (B.x - A.x) - (D.x - C.x) * (B.y - A.y);
+
+    if (bottom != 0) {
+      const t = tTop / bottom;
+      const u = uTop / bottom;
+      if (t >= 0 && t <= 1 && u >= 0 && u <= 1) {
+        return {
+          x: this.lerp(A.x, B.x, t),
+          y: this.lerp(A.y, B.y, t),
+          offset: t,
+        };
+      }
+    }
+
+    return null;
+  }
+
+  lerp(a: number, b: number, t: number) {
+    return a + (b - a) * t;
   }
 }
