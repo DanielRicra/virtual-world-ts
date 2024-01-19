7cad2305694e2e696d83c6100b74c5bfe4dc9257
DanielRicra <daniel.int@outlook.com>
2024-01-19 11:46:54 -0500

    Update polygon.ts
    Add new statics methods:
    
    -Union, containsSegment, containsPoint, break, multiBreak
    
    - drawSegments

--- src/primitives/polygon.ts
+++ src/primitives/polygon.ts
@@ -1,7 +1,104 @@
-import { Point } from ".";
+import { Point, Segment } from ".";
+import type { Utils } from "../math/utils";
 
 export class Polygon {
-  constructor(public points: Point[]) {}
+  segments: Segment[];
+
+  static union(polygons: Polygon[], utils: Utils): Segment[] {
+    Polygon.multiBreak(polygons, utils);
+    const keptSegments = [];
+
+    for (let i = 0; i < polygons.length; i++) {
+      for (const seg of polygons[i].segments) {
+        let keep = true;
+        for (let j = 0; j < polygons.length; j++) {
+          if (i != j) {
+            if (polygons[j].containsSegment(seg)) {
+              keep = false;
+              break;
+            }
+          }
+        }
+
+        if (keep) {
+          keptSegments.push(seg);
+        }
+      }
+    }
+
+    return keptSegments;
+  }
+
+  containsSegment(seg: Segment): boolean {
+    const midPoint = this.utils.average(seg.p1, seg.p2);
+    return this.containsPoint(midPoint);
+  }
+
+  containsPoint(point: Point): boolean {
+    const outerPoint = new Point(-1000, -1000);
+    let intersectionCount = 0;
+
+    for (const seg of this.segments) {
+      const int = this.utils.getIntersection(outerPoint, point, seg.p1, seg.p2);
+
+      if (int) {
+        intersectionCount++;
+      }
+    }
+
+    return intersectionCount % 2 === 1;
+  }
+
+  static multiBreak(polygons: Polygon[], utils: Utils) {
+    for (let i = 0; i < polygons.length - 1; i++) {
+      for (let j = i + 1; j < polygons.length; j++) {
+        Polygon.break(polygons[i], polygons[j], utils);
+      }
+    }
+  }
+
+  static break(polygon1: Polygon, polygon2: Polygon, utils: Utils): void {
+    const segments1 = polygon1.segments;
+    const segments2 = polygon2.segments;
+
+    for (let i = 0; i < segments1.length; i++) {
+      for (let j = 0; j < segments2.length; j++) {
+        const int = utils.getIntersection(
+          segments1[i].p1,
+          segments1[i].p2,
+          segments2[j].p1,
+          segments2[j].p2
+        );
+
+        if (int && int.offset != 1 && int.offset != 0) {
+          const point = new Point(int.x, int.y);
+          let aux = segments1[i].p2;
+          segments1[i].p2 = point;
+          segments1.splice(i + 1, 0, new Segment(point, aux));
+          aux = segments2[j].p2;
+          segments2[j].p2 = point;
+          segments2.splice(j + 1, 0, new Segment(point, aux));
+        }
+      }
+    }
+  }
+
+  constructor(public points: Point[], private readonly utils: Utils) {
+    this.segments = [];
+
+    for (let i = 1; i <= points.length; i++) {
+      this.segments.push(new Segment(points[i - 1], points[i % points.length]));
+    }
+  }
+
+  drawSegments(ctx: CanvasRenderingContext2D) {
+    for (const seg of this.segments) {
+      seg.draw(ctx, {
+        color: `hsl(${290 + Math.random() * 260}, 100%, 60%)`,
+        width: 5,
+      });
+    }
+  }
 
   draw(
     context: CanvasRenderingContext2D,
