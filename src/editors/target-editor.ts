d3f3dfb54a3f8b14532c6ee82990317df7dcc565
DanielRicra <daniel.int@outlook.com>
2024-01-28 18:02:27 -0500

    Add target-editor  and target marking

---
+++ src/editors/target-editor.ts
@@ -0,0 +1,23 @@
+import { Target } from "../markings";
+import { Marking } from "../markings/marking";
+import type { Utils } from "../math";
+import { Point } from "../primitives";
+import { Viewport } from "../viewport";
+import { World } from "../world";
+import { MarkingEditor } from "./marking-editor";
+
+export class TargetEditor extends MarkingEditor {
+  constructor(viewport: Viewport, world: World, utils: Utils) {
+    super(viewport, world, world.laneGuides, utils);
+  }
+
+  createMarking(center: Point, directionVector: Point): Marking {
+    return new Target(
+      center,
+      directionVector,
+      this.world.roadWidth / 2,
+      this.world.roadWidth / 2,
+      this.utils
+    );
+  }
+}
---
+++ src/markings/target.ts
@@ -0,0 +1,21 @@
+import type { Utils } from "../math";
+import { Point } from "../primitives";
+import { Marking } from "./marking";
+
+export class Target extends Marking {
+  constructor(
+    center: Point,
+    directionVector: Point,
+    width: number,
+    height: number,
+    utils: Utils
+  ) {
+    super(center, directionVector, width, height, utils);
+  }
+
+  draw(context: CanvasRenderingContext2D) {
+    this.center.draw(context, { color: "red", size: 30 });
+    this.center.draw(context, { color: "white", size: 20 });
+    this.center.draw(context, { color: "red", size: 10 });
+  }
+}
