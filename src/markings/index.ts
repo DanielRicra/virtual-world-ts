e246a751c9c7d69d63181bb2299dcebb7347ef7e
DanielRicra <daniel.int@outlook.com>
2024-01-28 18:02:53 -0500

    Update the exports in index.ts

--- src/markings/index.ts
+++ src/markings/index.ts
@@ -1,2 +1,7 @@
 export * from "./stop";
 export * from "./crossing";
+export * from "./start";
+export * from "./yield";
+export * from "./parking";
+export * from "./light";
+export * from "./target";
