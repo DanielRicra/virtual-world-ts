038895737be4dacd11f5ec052ee25935f91c170c
DanielRicra <daniel.int@outlook.com>
2024-01-28 14:25:46 -0500

    Refactor main.ts group the editors
    All the editor and its buttons are saved in a tools object

--- src/main.ts
+++ src/main.ts
@@ -1,18 +1,24 @@
 import { Graph } from "./math/graph";
 import config from "./config/constants";
 import "./style.css";
-import { GraphEditor } from "./graph-editor";
 import { Utils } from "./math/utils";
 import { Viewport } from "./viewport";
 import { World } from "./world";
+import { StopEditor, GraphEditor, CrossingEditor } from "./editors";
 
 const canvas = document.querySelector("#myCanvas") as HTMLCanvasElement;
 
 const deleteBtn = document.querySelector("#deleteBtn") as HTMLButtonElement;
 const saveBtn = document.querySelector("#saveBtn") as HTMLButtonElement;
+const setModeBtn = document.querySelector("#setModeBtn") as HTMLButtonElement;
+const stopBtn = document.querySelector("#stopBtn") as HTMLButtonElement;
+const crossingBtn = document.querySelector("#crossingBtn") as HTMLButtonElement;
 
 deleteBtn.addEventListener("click", dispose);
 saveBtn.addEventListener("click", save);
+setModeBtn.addEventListener("click", () => setMode("graph"));
+stopBtn.addEventListener("click", () => setMode("stop"));
+crossingBtn.addEventListener("click", () => setMode("crossing"));
 
 canvas.width = config.WIDTH;
 canvas.height = config.HEIGHT;
@@ -28,12 +34,24 @@ const graphInfo = graphString ? JSON.parse(graphString) : null;
 
 const utils = new Utils();
 const graph = graphInfo ? Graph.load(graphInfo, utils) : new Graph();
-const world = new World(graph, utils, 50, 10);
+const world = new World(graph, utils, 76, 10);
 
 const viewport = new Viewport(canvas, utils);
-const graphEditor = new GraphEditor(viewport, graph, utils);
+
+const tools = {
+  graph: {
+    button: setModeBtn,
+    editor: new GraphEditor(viewport, graph, utils),
+  },
+  stop: { button: stopBtn, editor: new StopEditor(viewport, world, utils) },
+  crossing: {
+    button: crossingBtn,
+    editor: new CrossingEditor(viewport, world, utils),
+  },
+};
 
 let oldGraphHad = graph.hash();
+setMode("graph");
 animate();
 
 function animate() {
@@ -45,14 +63,32 @@ function animate() {
   const viewPoint = utils.scale(viewport.getOffset(), -1);
   world.draw(context!, viewPoint);
   if (context) context.globalAlpha = 0.3;
-  graphEditor.display();
+
+  for (const tool of Object.values(tools)) {
+    tool.editor.display();
+  }
+
   requestAnimationFrame(animate);
 }
 
 function dispose() {
-  graphEditor.dispose();
+  tools.graph.editor.dispose();
+  world.markings.length = 0;
 }
 
 function save() {
   localStorage.setItem("graph", JSON.stringify(graph));
+}
+
+function setMode(mode: "graph" | "stop" | "crossing") {
+  disableEditors();
+  tools[mode].button.style.backgroundColor = "#8e03aa";
+  tools[mode].editor.enable();
+}
+
+function disableEditors() {
+  for (const tool of Object.values(tools)) {
+    tool.button.style.backgroundColor = "transparent";
+    tool.editor.disable();
+  }
 }
